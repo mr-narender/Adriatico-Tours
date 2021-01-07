8f1e4a78469adb39d55bc6dc4bd74d07fcf60478
Boki136 <boris.gersic1@gmail.com>
2021-01-07 02:02:21 +0000

    build[search-bar-mobile]: add functionality, apply styling. Improve responsiveness across devices

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,9 +5,6 @@ var count = 0;
 
 // Expand destinations link 
 
-$(document).ready(function () {
-
-
     $(".expand-lg").click(function () {
 
         count += 1;
@@ -24,9 +21,14 @@ $(document).ready(function () {
     var mobileMenu = $(".mobile_menu-expand");
     mobileMenu.width(0);
     $(".menu-open").click(function () {
+
+        $(".mobile-search").hide();
+        $(".search-expand").show();
+        $(".mobile_menu-content").css("margin-right", "auto");
+
         $(".level-one").show();
         $(mobileMenu).show().animate({
-            'width': '65%'
+            'width': '70%'
 
         }, 175);
     })
@@ -79,8 +81,13 @@ $(document).ready(function () {
     
 
       if(el.target == triggerDrop[0]) {
-        $(triggerDrop[0]).addClass("rotate");
         $(eachDestination[0]).show();
+        $(triggerDrop[0]).addClass("rotate");
+        $(eachDestination[1],eachDestination[2]).hide();
+        $(triggerDrop[1],triggerDrop[2]).removeClass("rotate");
+        
+       
+
 
             if (count % 2 == 0) {
             $(this).removeClass("rotate");
@@ -89,16 +96,21 @@ $(document).ready(function () {
         
       
     }   else if (el.target == triggerDrop[1]) {
-           $(triggerDrop[1]).addClass("rotate");
-        $(eachDestination[1]).show();
+           $(eachDestination[1]).show();
+        $(triggerDrop[1]).addClass("rotate");
+        $(eachDestination[0],eachDestination[2]).hide();
+        $(triggerDrop[0],triggerDrop[2]).removeClass("rotate");
+       
 
             if (count % 2 == 0) {
             $(this).removeClass("rotate");
             $(eachDestination[1]).hide();
         }
       }  else if (el.target == triggerDrop[2]) {
-           $(triggerDrop[2]).addClass("rotate");
-        $(eachDestination[2]).show();
+           $(eachDestination[2]).show();
+        $(triggerDrop[2]).addClass("rotate");
+        $(eachDestination[0],eachDestination[1]).hide();
+        $(triggerDrop[0],triggerDrop[1]).removeClass("rotate");
 
             if (count % 2 == 0) {
             $(this).removeClass("rotate");
@@ -110,7 +122,27 @@ $(document).ready(function () {
     
 });
 
+
+
+// Expand Search Mobile
+
+$(".search-expand").click(function() {
+
+    $(this).hide();
+    $(".mobile_menu-content").css("margin-right", "0");
+    $(".mobile-search").css({"display": "flex", "align-items": "center"});
+
 });
 
+
+// Close Search Mobile 
+
+$(".search-hide").click(function() {
+
+  $(".mobile-search").hide();
+  $(".search-expand").show();
+  $(".mobile_menu-content").css("margin-right", "auto");
+
+});
 
 
