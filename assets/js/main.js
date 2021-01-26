fccbb4811dfc7610d694f98a6b322dd0f52c923d
Boki136 <boris.gersic1@gmail.com>
2021-01-26 23:21:52 +0000

    add[most-popular detination section]: replicate most popular destination section from the homepage.

--- assets/js/main.js
+++ assets/js/main.js
@@ -219,3 +219,25 @@ $(dropdownTrigger).click(function () {
 
 
 
+// Currency Converter
+
+endpoint = 'live'
+access_key = '4a7335813869248c5349a15441119602';
+
+// get the most recent exchange rates via the "live" endpoint:
+$.ajax({
+    url: 'https://api.currencylayer.com/' + endpoint + '?access_key=' + access_key,   
+    dataType: 'jsonp',
+    success: function(json) {
+
+        // exchange rata data is stored in json.quotes
+        alert(json.quotes.USDGBP);
+        
+        // source currency is stored in json.source
+        alert(json.source);
+        
+        // timestamp can be accessed in json.timestamp
+        alert(json.timestamp);
+        
+    }
+});
